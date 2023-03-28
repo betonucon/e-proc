9356a4214268686d85c489b542f8f708bf3473d5
betonucon <betonucon@gmail.com>
2023-03-28 16:30:36 +0700

    push

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,28 +2,73 @@
 
 @section('content')
         <div id="content" class="content">
-			<!-- begin breadcrumb -->
-			<ol class="breadcrumb float-xl-right">
-				<li class="breadcrumb-item active">Home</li>
-			</ol>
-			<!-- end breadcrumb -->
-			<!-- begin page-header -->
-			<h1 class="page-header">Home <small>E-Procurement PT KPDP</small></h1>
-			<!-- end page-header -->
-			
-			<!-- begin panel -->
-			<div class="panel">
-				<div class="panel-heading" style="background:#dbf3db">
-					<h4 class="panel-title">Panel Title here</h4>
-					<div class="panel-heading-btn">
-						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
-						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-redo"></i></a>
-						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
-						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+			<div class="row">
+				<div class="col-md-1">
+
+				</div>
+				<div class="col-md-10">
+					<ol class="breadcrumb float-xl-right">
+						<li class="breadcrumb-item active">Home</li>
+					</ol>
+					<h1 class="page-header">Home <small>E-Procurement PT KPDP</small></h1>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="panel">
+								<div class="panel-body" style="padding: 3px;">
+									<img src="{{url_plug()}}/img/kpdp.jpeg" width="100%" height="200px">
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="panel">
+								<div class="panel-body text-center ">
+									<span class="fa-stack fa-2x fa-4x text-aqua">
+										<i class="far fa-circle fa-stack-2x"></i>
+										<i class="fa fa-clone fa-stack-1x"></i>
+									</span><br><br>
+									<h5>Panduan Pengguna</h5>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="panel">
+								<div class="panel-body text-center ">
+									<span class="fa-stack fa-2x fa-4x text-aqua">
+										<i class="far fa-circle fa-stack-2x"></i>
+										<i class="fa fa-question fa-stack-1x"></i>
+									</span><br><br>
+									<h5>Faq</h5>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="panel">
+								<div class="panel-body text-center ">
+									<span class="fa-stack fa-2x fa-4x text-aqua">
+										<i class="far fa-circle fa-stack-2x"></i>
+										<i class="fa fa-info fa-stack-1x"></i>
+									</span><br><br>
+									<h5>Kebijakan</h5>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="panel">
+								<div class="panel-body text-center ">
+									<span class="fa-stack fa-2x fa-4x text-aqua">
+										<i class="far fa-circle fa-stack-2x"></i>
+										<i class="fa fa-phone fa-stack-1x"></i>
+									</span><br><br>
+									<h5>Hubungi Kami</h5>
+								</div>
+							</div>
+						</div>
+						
+						
 					</div>
 				</div>
-				<div class="panel-body">
-					Panel Content Here
+				<div class="col-md-1">
+
 				</div>
 			</div>
 			<!-- end panel -->
